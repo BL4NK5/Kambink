34d4c839b4d51c8e42c32af20769b25301b86d9d
BL4NK <47903330+BL4NK5@users.noreply.github.com>
2019-07-20 16:41:42 +0000

    Add files via upload

---
+++ warna.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/sh
+
+###COLOURR###
+black='\x1b[1;30m'
+ungu='\x1b[1;35m'
+red='\x1b[1;31m'
+green='\x1b[1;32m'
+blue='\x1b[1;34m'
+yellow='\x1b[1;33m'
+cyan='\x1b[1;36m'
+default='\x1b[0m'
+
+clear
+echo -e $red"kode R"
+sleep 1
+echo -e $green"kode G"
+sleep 1
+echo -e $blue"kode B"
+sleep 1
+echo -e $yellow"kode Y"
+sleep 1
+echo -e $cyan"kode C"
+sleep 1
+echo -e $default"kode D"
+sleep 1
+echo -e $ungu" kode t"
+sleep 120
+clear
+exit
